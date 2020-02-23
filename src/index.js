8c509cbee02ce0ae99fc3b7d0f0fecbea64b557b
Jesse Gonzalez <jessegonzalez024@gmail.com>
2020-02-23 23:08:29 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -2,12 +2,27 @@
 //   // your code here
 // });
 
+// Stores the form HTML element
+const form = document.getElementById('create-task-form');
 
-const form = document.getElementById("create-task-form")
+let tasks = document.getElementById('tasks') 
 
+let newToDo = document.getElementById('new-task-description');
 
 form.addEventListener('submit', function(event) {
-  event.preventDefault();
   
+   event.preventDefault();
+   
+   let toDo = newToDo.value;
+   
+   let toDoItem = document.createElement('li');
+   
+   let toDoItemText = document.createTextNode(toDo);
+   
+   toDoItem.appendChild(toDoItemText);
+   
+   tasks.appendChild(toDoItem);
+   
+   newToDo.value = "";
+   
 });
-
