85db5137ea823ec805f62351290cd8b2938b4b9c
Jesse Gonzalez <jessegonzalez024@gmail.com>
2020-02-23 23:10:03 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -5,8 +5,10 @@
 // Stores the form HTML element
 const form = document.getElementById('create-task-form');
 
+//Stores the HTML <ul> tag. Items created would be displayed as <li>
 let tasks = document.getElementById('tasks') 
 
+// Stores 
 let newToDo = document.getElementById('new-task-description');
 
 form.addEventListener('submit', function(event) {
